21f4a521e34dff23da339c9f808e6dc86dd77b2f
shan <zahoorzeeshan1@gmail.com>
2017-03-11 00:19:27 +0300

    workaround paging

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,26 @@
 class HomeController < ApplicationController
   def index
     leaguename = params[:leaguename]
+    pageindex = params[:page]
+    pageindex = 0 unless pageindex
     if leaguename
       league = Competition.where(:CanonicalName => leaguename).first()
       if league
-       @Matches = league.matches
+       @Matches = league.matches.offset(pageindex*1).limit(1)
       end
     else
-      @Matches = Match.take(10)
+      @Matches = Match.offset(pageindex*1).take(1)
     end
-
+    if request.xhr?
+      render "loadmoreIndex", :layout=> false
+    end
+  end
+  def loadmoreIndex
+    leaguename = params[:leaguename]
+    @Matches = Match.take(1)
+    if request.xhr?
+      render :layout=> false
+    end
   end
   def play
     @team1 = params[:team1]
